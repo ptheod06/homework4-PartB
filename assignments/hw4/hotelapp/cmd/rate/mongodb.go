8fe5a51339b3e59e7bf8f7f963bfbca91ce20307
ptheod <ptheod@node0.ptheo06-assn4.ucy-cs499-dc-pg0.wisc.cloudlab.us>
2022-12-20 10:24:25 -0600

    final commit

--- assignments/hw4/hotelapp/cmd/rate/mongodb.go
+++ assignments/hw4/hotelapp/cmd/rate/mongodb.go
@@ -11,10 +11,11 @@ import (
 
 var (
 	database_addr = flag.String("db_addr", "mongodb-rate:27017", "Address of the data base server")
+	memcache_addr = flag.String("memc_addr", "memcached-profile:11211", "Address of the memcache server")
 )
 
 func initializeRateDatabase() *rate.DatabaseSession {
-	dbsession := rate.NewDatabaseSession(*database_addr)
+	dbsession := rate.NewDatabaseSession(*database_addr, *memcache_addr)
 	dbsession.LoadDataFromJsonFile(filepath.Join(*jsonDataDir, "inventory.json"))
 	return dbsession
 }
--- assignments/hw4/hotelapp/internal/rate/mongodb.go
+++ assignments/hw4/hotelapp/internal/rate/mongodb.go
@@ -3,19 +3,43 @@
 package rate
 
 import (
+	"encoding/json"
 	log "github.com/sirupsen/logrus"
 
+	pb "github.com/ucy-coast/hotel-app/internal/rate/proto"
 	"github.com/ucy-coast/hotel-app/pkg/util"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+
+	"time"
+
+	"github.com/bradfitz/gomemcache/memcache"
+
 )
 
 type DatabaseSession struct {
+
 	MongoSession *mgo.Session
+	MemcClient   *memcache.Client
 }
 
-func NewDatabaseSession(db_addr string) *DatabaseSession {
+func NewDatabaseSession(db_addr string, memc_addr string) *DatabaseSession {
 	// TODO: Implement me
+
+	session, err := mgo.Dial(db_addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info("New session successfull...")
+
+	memc_client := memcache.New(memc_addr)
+	memc_client.Timeout = time.Second * 2
+	memc_client.MaxIdleConns = 512
+
+	return &DatabaseSession{
+		MongoSession: session,
+		MemcClient: memc_client,
+	}
 }
 
 func (db *DatabaseSession) LoadDataFromJsonFile(rateJsonPath string) {
@@ -25,4 +49,58 @@ func (db *DatabaseSession) LoadDataFromJsonFile(rateJsonPath string) {
 // GetRates gets rates for hotels for specific date range.
 func (db *DatabaseSession) GetRates(hotelIds []string) (RatePlans, error) {
 	// TODO: Implement me
+
+
+	rates := make([]*pb.RatePlan, 0)
+
+	for _, id := range hotelIds {
+		// first check memcached
+
+		item, err := db.MemcClient.Get(id)
+		if err == nil {
+			// memcached hit
+			log.Infof("Memcached hit: hotel_id == %v\n", id)
+			hotel_rate := new(pb.RatePlan)
+			if err = json.Unmarshal(item.Value, hotel_rate); err != nil {
+				log.Warn(err)
+			}
+			rates = append(rates, hotel_rate)
+		} else if err == memcache.ErrCacheMiss {
+			// memcached miss, set up mongo connection
+			log.Infof("Memcached miss: hotel_id == %v\n", id)
+			session := db.MongoSession.Copy()
+			defer session.Close()
+			c := session.DB("rate-db").C("inventory")
+		
+			hotel_rate := new(pb.RatePlan)
+			err := c.Find(bson.M{"id": id}).One(&hotel_rate)
+			if err != nil {
+				log.Fatalf("Failed get hotels rate plan: ", err)
+			}
+			rates = append(rates, hotel_rate)
+
+			rate_json, err := json.Marshal(hotel_rate)
+			if err != nil {
+				log.Errorf("Failed to marshal hotel rate [id: %v] with err:", hotel_rate.HotelId, err)
+			}
+			memc_str := string(rate_json)
+
+			// write to memcached
+			err = db.MemcClient.Set(&memcache.Item{Key: id, Value: []byte(memc_str)})
+			if err != nil {
+				log.Warn("MMC error: ", err)
+			}
+		} else {
+			log.Errorf("Memcached error = %s\n", err)
+			panic(err)
+		}	
+	}
+
+
+
+
+
+	return rates, nil
+
+
 }
